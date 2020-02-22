97773b10ae987d704210f0ed192bf7ab0cdd727c
krishnga <ganesh.krishnamurthy@morganstanley.com>
2020-02-23 03:52:15 +0530

    build: deploy compose yml

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,7 +2,9 @@
 pwd
 cd /var/www/docker-blog
 pwd
+docker-compose down
 # echo $DOCKER_PASSWORD | docker login -u $DOCKER_ID --password-stdin
 docker pull jodishwaran/ganesh-blog:next-app
 docker pull jodishwaran/ganesh-blog:nginx
+docker-compose up
 pwd
