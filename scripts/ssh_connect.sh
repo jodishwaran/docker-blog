3c3e48d9078867e3d8395d874285456dfe9971b4
krishnga <ganesh.krishnamurthy@morganstanley.com>
2020-02-23 02:42:45 +0530

    build : travis

--- scripts/ssh_connect.sh
+++ scripts/ssh_connect.sh
@@ -7,5 +7,5 @@ eval "$(ssh-agent -s)"
 ssh-add ~/.ssh/travis
 # ssh-keyscan -H 'gitlab.com' >> ~/.ssh/known_hosts
 scp docker-compose.yml $DO_TRAVIS_CI_USER@$DO_HOST:$DO_APP_DIR
-chmod +x ./deploy.sh
-ssh $DO_TRAVIS_CI_USER@$DO_HOST 'bash -s' < ./deploy.sh
+# chmod +x ./deploy.sh
+# ssh $DO_TRAVIS_CI_USER@$DO_HOST 'bash -s' < ./deploy.sh
