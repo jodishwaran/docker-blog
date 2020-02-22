c10e073c367cfe19ab42df99c494a97deb2466a7
krishnga <ganesh.krishnamurthy@morganstanley.com>
2020-02-23 03:28:19 +0530

    build: travis change

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
+pwd
 cd $DO_APP_DIR
-echo $DOCKER_PASSWORD | docker login -u $DOCKER_ID --password-stdin
+# echo $DOCKER_PASSWORD | docker login -u $DOCKER_ID --password-stdin
 docker pull $DOCKER_REPO:next-app
 docker pull $DOCKER_REPO:nginx
