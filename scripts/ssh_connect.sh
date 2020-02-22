4cfea78660cf8a88bb77f6819606464185f28661
krishnga <ganesh.krishnamurthy@morganstanley.com>
2020-02-23 02:38:49 +0530

    build: travis rsa change

--- scripts/ssh_connect.sh
+++ scripts/ssh_connect.sh
@@ -2,9 +2,9 @@
 mkdir -p ~/.ssh
 # echo $DO_TRAVIS_CI_USER_PK | tr -d '\r' > ~/.ssh/id_rsa
 # cat ~/.ssh/id_rsa
-chmod 700 ~/.ssh/travis_rsa
+chmod 700 ~/.ssh/travis
 eval "$(ssh-agent -s)"
-ssh-add ~/.ssh/travis_rsa
+ssh-add ~/.ssh/travis
 # ssh-keyscan -H 'gitlab.com' >> ~/.ssh/known_hosts
 scp docker-compose.yml $DO_TRAVIS_CI_USER@$DO_HOST:$DO_APP_DIR
 chmod +x ./deploy.sh
