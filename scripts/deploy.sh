a35fef2129603dffa4bdb06adb06c1c3d568a2bf
krishnga <ganesh.krishnamurthy@morganstanley.com>
2020-02-23 04:08:33 +0530

    build screwd cert

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -6,5 +6,7 @@ docker-compose down
 # echo $DOCKER_PASSWORD | docker login -u $DOCKER_ID --password-stdin
 docker pull jodishwaran/ganesh-blog:next-app
 docker pull jodishwaran/ganesh-blog:nginx
+docker volume rm docker-blog_web-root
+docker volume rm docker-blog_dhparam
 docker-compose up
 pwd
