77094ed72719bbf2dcd293ca23919018cec93b55
krishnga <ganesh.krishnamurthy@morganstanley.com>
2020-02-23 11:47:30 +0530

    build nginx secure config

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -2,11 +2,9 @@
 pwd
 cd /var/www/docker-blog
 pwd
-docker-compose down
+docker-compose stop nginx
 # echo $DOCKER_PASSWORD | docker login -u $DOCKER_ID --password-stdin
 docker pull jodishwaran/ganesh-blog:next-app
 docker pull jodishwaran/ganesh-blog:nginx
-docker volume rm docker-blog_web-root
-docker volume rm docker-blog_dhparam
-docker-compose up
+docker-compose up --force-recreate --no-deps nginx
 pwd
