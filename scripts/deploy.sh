8a99b48d0449ba5f7e686ec47e3c2a7bc5469736
krishnga <ganesh.krishnamurthy@morganstanley.com>
2020-02-23 20:25:17 +0530

    build CI-CD test

--- scripts/deploy.sh
+++ scripts/deploy.sh
@@ -1,10 +1,8 @@
 #!/bin/bash
-pwd
 cd /var/www/docker-blog
-pwd
-docker-compose stop nginx
+# docker-compose stop nginx
 # echo $DOCKER_PASSWORD | docker login -u $DOCKER_ID --password-stdin
 docker pull jodishwaran/ganesh-blog:next-app
 docker pull jodishwaran/ganesh-blog:nginx
-docker-compose up --force-recreate --no-deps nginx
-pwd
+# docker-compose up --force-recreate --no-deps nginx
+docker-compose restart next-app
